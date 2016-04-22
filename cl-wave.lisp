2bf7561fae8314af2277a0b2a876e87244d61aa6
Ryan King <hello@ryanking.com>
2016-04-21 23:03:19 -0400

    Functioning uint and tag readers

--- cl-wave.lisp
+++ cl-wave.lisp
@@ -4,4 +4,20 @@
 
 (in-package #:cl-wave)
 
+(defun read-uint (stream n)
+  "Reads an n-byte unsigned little-endian integer from stream."
+  (loop for i below n
+     with uint = 0 do
+       (setf (ldb (byte 8 (* i 8)) uint) (read-byte stream nil))
+     finally (return uint)))
+
+(defun read-tag (stream)
+  "Reads a four character tag from the stream and returns a string
+   or nil if its the end of the file."
+  (loop repeat 4
+     for byte = (read-byte stream nil)
+     while byte
+     collecting (code-char byte) into chars
+     finally
+       (when chars (return (coerce chars 'string)))))
 
