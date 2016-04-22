068b075782c98f7a7a100f69ef2f6e6fccfc92b3
Ryan King <hello@ryanking.com>
2016-04-22 01:10:35 -0400

    Finish basic wave reading functionality

--- cl-wave.lisp
+++ cl-wave.lisp
@@ -7,9 +7,9 @@
 (defun read-uint (stream n)
   "Reads an n-byte unsigned little-endian integer from stream."
   (loop for i below n
-     with uint = 0 do
-       (setf (ldb (byte 8 (* i 8)) uint) (read-byte stream nil))
-     finally (return uint)))
+        with uint = 0 do
+          (setf (ldb (byte 8 (* i 8)) uint) (read-byte stream))
+          finally (return uint)))
 
 (defun read-tag (stream)
   "Reads a four character tag from the stream and returns a string
@@ -21,3 +21,43 @@
      finally
        (when chars (return (coerce chars 'string)))))
 
+(defun make-wave ()
+  (list :file-size 0
+        :file-type ""
+        :fmt-size 0
+        :compression-code 0
+        :number-of-channels 0
+        :sample-rate 0
+        :bytes-per-second 0
+        :bytes-per-sample 0
+        :bits-per-sample 0
+        :data-size 0
+        :frames '()))
+
+(defun read-wave (stream)
+  (loop for chunk-id = (read-tag stream)
+     with wave = (make-wave)
+       with others = 0
+     while chunk-id do
+       (cond ((equal chunk-id "RIFF")
+              (setf (getf wave :file-size) (read-uint stream 4))
+              (setf (getf wave :file-type) (read-tag stream)))
+             ((equal chunk-id "fmt ")
+              (setf (getf wave :fmt-size) (read-uint stream 4))
+              (setf (getf wave :compression-code) (read-uint stream 2))
+              (setf (getf wave :number-of-channels) (read-uint stream 2))
+              (setf (getf wave :sample-rate) (read-uint stream 4))
+              (setf (getf wave :bytes-per-second) (read-uint stream 4))
+              (setf (getf wave :bytes-per-sample) (read-uint stream 2))
+              (setf (getf wave :bits-per-sample) (read-uint stream 4)))
+             ((equal chunk-id "data")
+              (let* ((bps (getf wave :bytes-per-sample))
+                     (diff (expt 2 (getf wave :bits-per-sample)))
+                     (max (1- (/ diff 2)))
+                     (data-size (read-uint stream 4)))
+                (setf (getf wave :data-size) data-size)
+                (setf (getf wave :frames)
+                      (loop for frame = (read-uint stream bps)
+                         repeat (/ data-size bps)
+                         collect (if (> frame max) (- frame diff) frame))))))
+       finally (return wave)))
